e1d0a45671c2840c8d3bb90ab1bb9f26d7dace57
ashokrajpaulraj <ashokrajkp@gmail.com>
2022-02-01 10:38:24 +0530

    adding new route called contact

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get("/about", function(req, res){
     res.send("About us page")
 })
 
+app.get("/contact", function(req, res){
+    res.send("Contact page")
+})
+
 app.listen(port, function(err){
     console.log("server started at port ", port)
 })
