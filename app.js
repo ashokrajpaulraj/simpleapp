ee9ef34ca033a0335d53c12028cc560ff9ea1277
ashokrajpaulraj <ashokrajkp@gmail.com>
2022-02-01 09:24:13 +0530

    i have added a new route called about

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get("/", function(req, res){
     res.send("Welcome page")
 })
 
+app.get("/about", function(req, res){
+    res.send("About us page")
+})
+
 app.listen(port, function(err){
     console.log("server started at port ", port)
 })
